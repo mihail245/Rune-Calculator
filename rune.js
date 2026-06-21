8d8101f40d2804d5dfccef49b30e6b244c844b1f
mihail245 <privetbrawl3@gmail.com>
2026-06-21 21:56:04 +0300

    Create rune.js

---
+++ rune.js
@@ -0,0 +1,15 @@
+// База данных рун из изображения 1000567328.png
+const runesDatabase = [
+    { name: "Rookie", chance: 1.25, displayChance: "1/1.25", color: "#e1cbff" },
+    { name: "Learner", chance: 6.67, displayChance: "1/6.67", color: "#00e600" },
+    { name: "Trained", chance: 10, displayChance: "1/10", color: "#0080ff" },
+    { name: "Skilled", chance: 10, displayChance: "1/10", color: "#b300b3" },
+    { name: "Expert", chance: 10, displayChance: "1/10", color: "#ffaa00" },
+    { name: "Master", chance: 10, displayChance: "1/10", color: "#ff0000" },
+    { name: "Grandmaster", chance: 10, displayChance: "1/10", color: "#ff5500" },
+    { name: "Celestial", chance: 10, displayChance: "1/10", color: "#ff00ff" },
+    { name: "Immortal", chance: 115990000000, displayChance: "1/115.99B", color: "#00bfff" },
+    { name: "Shadow", chance: 1e21, displayChance: "1/10Sx", color: "#4b0082" },
+    { name: "Phantom", chance: 1e29, displayChance: "1/100Oc", color: "#ff007f" },
+    { name: "Atomic", chance: 1.33e60, displayChance: "1/1.33QnDe", color: "#9400d3" }
+];
